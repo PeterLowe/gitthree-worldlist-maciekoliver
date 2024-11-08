4d2d8632eb38a24328a7801eb4d5152506ddece9
(Student  C00311971) Oliver Junca <C00311971@setu.ie>
2024-11-08 11:49:43 +0000

    Oliver wrote Y,X,W,V, & U.

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -55,6 +55,102 @@ int main()
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
 	}	
+	if (letter == 'u' || letter == 'U') // Written by Oliver Junca
+	{
+		if (region == 1)
+		{
+			std::cout << "United States of America, Uruguay." << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "United Kingdom, Ukraine." << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Uganda" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "United Arab Emirates, Uzbekistan." << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no Us in the rest of the world." << std::endl;
+		}
+	}
+	if (letter == 'v' || letter == 'V') // Written by Oliver Junca
+	{
+		if (region == 1)
+		{
+			std::cout << "Venezuela, Virgin Islands (British), Virgin Islands (US)." << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "Vatican City State (Holy See)." << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Vs in Africa." << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "Vietnam." << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Vanuatu." << std::endl;
+		}
+	}
+	if (letter == 'w' || letter == 'W') // Written by Oliver Junca
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Ws in the Americas." << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ws in Europe." << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "Western Sahara." << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Ws in Asia." << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Wallis and Futana Islands." << std::endl;
+		}
+	}
+	if (letter == 'x' || letter == 'X') // Written by Oliver Junca
+	{
+		std::cout << "There are no Xs in any country of the world." << std::endl;
+	}
+	if (letter == 'y' || letter == 'Y') // Written by Oliver Junca
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Ys in the Americas." << std::endl;
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ys in Europe." << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Ys in Africa." << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "Yemen." << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "There are no Ys in the rest of the world." << std::endl;
+		}
+	}
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
 		if (region == 1)
